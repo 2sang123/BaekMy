c0e5a35e8ba51fefde320c1feac5e60ad2ed84ac
Sang ki <81751601+2sang123@users.noreply.github.com>
2025-06-05 15:48:59 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 2936 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int n;
+bool visited[100005] = { false, };
+vector<pair<int, int>> graph[10005];
+int cnt = 0;
+int max_dist = 0;
+int far = 0;
+void dfs(int cur, int d)
+{
+	if (visited[cur]) return;
+	if (d > max_dist)
+	{
+		max_dist = d;
+		far = cur;
+	}
+
+	visited[cur] = true;
+	for (auto [next, dist] : graph[cur])
+	{
+		if (!visited[next])
+		{
+			dfs(next, d + dist);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n;
+
+	for (int i = 0; i < n-1; i++)
+	{
+		int u, v, w;
+		cin >> u >> v >> w;
+		graph[u].push_back({ v,w });
+		graph[v].push_back({ u,w });
+	}
+
+	dfs(1, 0);
+	memset(visited, false, sizeof(visited));
+	max_dist = 0;
+	dfs(far, 0);
+	cout << max_dist;
+}
