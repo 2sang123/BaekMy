72175e3160e0689fbc8e4c38fa7bada56c8f8d08
Sang ki <81751601+2sang123@users.noreply.github.com>
2025-07-27 13:56:54 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = n; j > i; j--)
+		{
+			cout << '*';
+		}
+		cout << '\n';
+	}
+}
