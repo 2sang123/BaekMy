f8200209031bd54179c1a17345cebdb724c53c22
Sang ki <81751601+2sang123@users.noreply.github.com>
2025-09-27 16:34:27 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int n;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+	if (n % 7 == 0||n%7==2)
+	{
+		cout << "CY";
+	}
+	else
+	{
+		cout << "SK";
+	}
+}
